e6d35d9105f37c7ccbab3f76ee6fd3cfe68d7d09
Chris <gateway.kranz@gmail.com>
2018-05-16 16:35:11 +0200

    Sets

--- app.js
+++ app.js
@@ -199,3 +199,50 @@
 // // Create an array of keys only
 // const keyArr = Array.from(map1.keys());
 // console.log(keyArr);
+
+
+// -------------------------------- Sets ----------------------------------------
+
+// SETS - Store unique values of any type
+
+const set1 = new Set();
+
+// Add values to Set
+set1.add(100);
+set1.add('a string');
+set1.add({name: 'John'});
+set1.add(true);
+set1.add(100);
+
+console.log(set1);
+
+const set2 = new Set([1, true, 'string']);
+console.log(set2);
+
+// Get count
+console.log(set1.size);
+
+// Check for values
+console.log(set1.has(100));
+console.log(set1.has(50+50));
+console.log(set1.has({name: 'John'})); // ==> false
+console.log({name: 'John'} === {name: 'John'}); // muss so weil kein primitive type
+
+// Delete from Set
+set1.delete(100);
+console.log(set1);
+
+// Iterate through sets
+// for...of
+for (let item of set1) {
+    console.log(item);
+}
+
+// forEach
+set1.forEach((value) => {
+    console.log(value);
+})
+
+// Covert into array
+const setArr = Array.from(set1);
+console.log(setArr);
